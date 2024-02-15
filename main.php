33be5b14e7d114f81b086257206ad2b442c7dc71
salsabillaputri0 <2223612.salsabilla@smkn-2sbg.sch.id>
2024-02-15 20:51:27 +0700

    strings

---
+++ main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="box">
+        <div class="atas"><h1>DATA DIRI</h1></div>
+        <div class="kiri">
+            <div class="gambar"></div>
+        </div>
+        <div class="kanan">
+            <pre> Nama:     Salsabilla Putri Ranesti<hr></pre>
+            <pre> Nit:      2223612<hr></pre>
+            <pre> TTL:      09-Januari-2007<hr></pre>
+            <pre> Jurusan:  RPL<hr></pre>
+            <pre> Tgl Pengumpulan: 6 Februari<hr></pre>
+            <pre> Alamat:   Kalijati Barat<hr></pre>
+<?php
+echo "Hello";
+print "Hello";
+?>
+</div>
+</div>
+</body>
+</html>
